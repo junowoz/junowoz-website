e5232cc0f07c4c6cfe6343401772b236351eb19a
Juan José <junogouvea@gmail.com>
2023-01-17 17:48:23 -0400

    new portfolio on next

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import "../styles/construction.css";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <head />
+            <body>{children}</body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import "../styles/construction.css";
+
+export default function Home() {
+    return (
+        <div className="container">
+            <h1>Em Construção 🚧👷‍♂️</h1>
+            <p>
+                Desculpe pelo inconveniente, estou trabalhando em uma melhor
+                versão do site para melhorar a sua experiência.
+            </p>
+            <p>Por favor, volte mais tarde.</p>
+        </div>
+    );
+}
