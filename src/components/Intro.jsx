070d48e686296cb797cbe0d12821135732fcb048
Juan José <junogouvea@gmail.com>
2023-01-28 21:59:06 -0400

    react minimal portfolio

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React from 'react';
 
-export default function Intro() {
-    return (
-        <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Junowoz</h1>
-            <h4>Software Developer</h4>
-        </div>
-    );
-};
+function Intro() {
+   return (
+      <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+         <h2 className="text-3xl md:text-6xl dark:text-white mb-1 md:mb-3 font-bold">Juan José Gouvêa</h2>
+         <p className="text-base md:text-xl mb-3 font-medium">Software Developer</p>
+      </div>
+   )
+}
+
+export default Intro;
