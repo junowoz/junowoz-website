5748ca339bbb07bcc349e53d8e595dd9bfaaaf1f
Juan José <junogouvea@gmail.com>
2023-01-29 13:20:51 -0400

    deploy

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,20 +1,20 @@
 export default [
-   {
-      title: 'Weather App',
-      imgUrl: '/assets/weather-react.png',
-      stack: ['React', 'JavaScript', 'REST API'],
-      link: 'https://fkcodes.com',
-   },
-   {
-      title: 'ChatGPT Clone',
-      imgUrl: '/assets/chatgpt-clone.png',
-      stack: ['React', 'Tailwind', 'Vite'],
-      link: 'https://thehackershideout.com',
-   },
-   {
-      title: 'Blog',
-      imgUrl: '/assets/blog.png',
-      stack: ['NextJS', 'TailwindCss', 'Markdown'],
-      link: 'https://youtu.be/A8eegxL82PI',
-   },
-]
+    {
+        title: "Weather App",
+        imgUrl: "/assets/weather-react.png",
+        stack: ["React", "JavaScript", "REST API"],
+        link: "https://weather-react-psi.vercel.app/",
+    },
+    {
+        title: "ChatGPT Clone",
+        imgUrl: "/assets/chatgpt-clone.png",
+        stack: ["React", "Tailwind", "Vite"],
+        link: "https://thehackershideout.com",
+    },
+    {
+        title: "Blog",
+        imgUrl: "/assets/blog.png",
+        stack: ["NextJS", "TailwindCss", "Markdown"],
+        link: "https://youtu.be/A8eegxL82PI",
+    },
+];
