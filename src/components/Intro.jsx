b60c5ed24e00eab3e9f9d0930f85b89dcb4ac33d
Juan José <junogouvea@gmail.com>
2023-01-30 02:45:18 -0400

    edits

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,9 +3,8 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <img src="logo.png" alt="junowoz" className="mb-3" />
             <p className="text-4xl md:text-5xl dark:text-white mb-1 md:mb-3 font-bold">
-                Juan José Gouvêa
+                Juan José Gouvêa 🌐
             </p>
             <p className="text-3xl md:text-3xl mb-3 font-medium">
                 Software Developer
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,14 +4,14 @@ export default [
         imgUrl: "assets/weather-react.png",
         stack: ["React", "JavaScript", "GeoDB API"],
         link: "https://weather-react-psi.vercel.app/",
-        repo: "https://github.com/tu-usuario/weather-app",
+        repo: "https://github.com/junowoz/weather-react",
     },
     {
         title: "ChatGPT Clone",
         imgUrl: "assets/chatgpt-clone.png",
         stack: ["React", "Tailwind", "Vite"],
         link: "https://thehackershideout.com",
-        repo: "https://github.com/tu-usuario/weather-app",
+        repo: "https://github.com/junowoz/chatgpt-clone",
     },
     {
         title: "Blog",
@@ -19,5 +19,12 @@ export default [
         stack: ["NextJS", "TailwindCss", "Markdown"],
         link: "https://youtu.be/A8eegxL82PI",
         repo: "https://github.com/tu-usuario/weather-app",
+    },
+    {
+        title: "Solidity Fundamentals Tutorial",
+        imgUrl: "assets/solidity.png",
+        stack: ["Solidity"],
+        link: "https://github.com/junowoz/solidity",
+        repo: "https://github.com/junowoz/solidity/blob/main/0_Solidity_Fundamentals.sol",
     },
 ];
