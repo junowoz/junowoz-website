1e67cf896697a65500f8c2260d00ff3b0c74da2c
Juan José <junogouvea@gmail.com>
2023-02-01 23:57:18 -0400

    port

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,19 +1,54 @@
 import React from "react";
 import portfolio from "../data/portfolio";
-import PortfolioItem from "./PortfolioItem";
 
 function Portfolio() {
     return (
         <div className="flex flex-col md:flex-row items-center justify-center">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 {portfolio.map((project) => (
-                    <PortfolioItem
-                        imgUrl={project.imgUrl}
-                        title={project.title}
-                        stack={project.stack}
-                        link={project.link}
-                        repo={project.repo}
-                    />
+                    <div className="border-2 border-stone-900 dark:border-white rounded-md overflow-hidden">
+                        <img
+                            src={project.imgUrl}
+                            alt="portfolio"
+                            className="w-full h-36 md:h-48 object-cover cursor-pointer"
+                        />
+
+                        <div className="w-full p-4">
+                            <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">
+                                {project.title}
+                            </h3>
+                            <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-white ">
+                                {project.stack.map((item) => (
+                                    <span className="inline-block px-2 py-1 font-semibold border-2 border-stone-900 dark:border-white rounded-md">
+                                        {item}
+                                    </span>
+                                ))}
+                            </p>
+
+                            <div className="flex flex-wrap gap-2 flex-row items-center justify-start mt-4">
+                                <a
+                                    href={project.repo}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="inline-block items-center px-3 py-2 border-2 border-stone-900 dark:border-white rounded-md hover:bg-stone-900 dark:hover:bg-white hover:text-white dark:hover:text-stone-900 transition-colors duration-200 ease-in-out"
+                                >
+                                    <span className="font-medium dark:text-white">
+                                        Code
+                                    </span>
+                                </a>
+                                <a
+                                    href={project.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="inline-block items-center px-3 py-2 border-2 border-stone-900 dark:border-white rounded-md hover:bg-stone-900 dark:hover:bg-white hover:text-white dark:hover:text-stone-900 transition-colors duration-200 ease-in-out"
+                                >
+                                    <span className="font-medium dark:text-white">
+                                        Live
+                                    </span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
--- src/data/skills.js
+++ src/data/skills.js
@@ -1,11 +1,18 @@
  export default [
-     { name: "React" },
-     { name: "Next" },
+     { name: "React.js" },
+     { name: "Next.js" },
      { name: "TypeScript" },
      { name: "JavaScript" },
-     { name: "HTML5" },
-     { name: "CSS3" },
-     { name: "TailwindCSS" },
+     { name: "Solidity" },
+     { name: "Tailwind CSS/CSS3/HTML5" },
+     { name: "Node.js" },
+     { name: "MySQL" },
+     { name: "SQL" },
+     { name: "RESTful APIs" },
+     { name: "Docker" },
      { name: "Git & Github" },
+     { name: "Scrum" },
+     { name: "Figma" },
+     { name: "Linux" }
  ];
 
