e18182ad7fd587b897122b33993b61303e32c41f
Juan José <junogouvea@gmail.com>
2023-02-06 14:27:14 -0400

    changes intro +favicon ico

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,10 +3,14 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-            <p className="text-3xl md:text-3xl font-medium">
-                🌐    
+            <p className="text-5xl md:text-5xl">
+                <img
+                    className="h-20 w-20"
+                    src="https://em-content.zobj.net/thumbs/120/whatsapp/326/globe-with-meridians_1f310.png"
+                    alt=""
+                />
             </p>
-            </br>
+            <br />
             <p className="text-4xl md:text-5xl dark:text-white mb-3 md:my-3 font-bold">
                 Juan José Gouvêa
             </p>
