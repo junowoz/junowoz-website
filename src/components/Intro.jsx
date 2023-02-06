ac032426d81dd3f5933675af3eef7e81ff92d020
Juan José <86818341+junowoz@users.noreply.github.com>
2023-02-06 13:48:39 -0400

    changes intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -3,8 +3,12 @@ import React from "react";
 function Intro() {
     return (
         <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+            <p className="text-3xl md:text-3xl font-medium">
+                🌐    
+            </p>
+            </br>
             <p className="text-4xl md:text-5xl dark:text-white mb-3 md:my-3 font-bold">
-                Juan José Gouvêa 🌐
+                Juan José Gouvêa
             </p>
             <p className="text-3xl md:text-3xl font-medium">
                 Software Developer
