700d8fcd6389eb355885e43d3281cf5a0a309e95
junowoz <junogouvea@gmail.com>
2023-02-26 21:52:27 -0400

    packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,16 @@
 module.exports = {
-	content: ['./index.html', './src/**/*.{jsx, js}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				inter: ['inter', 'serif'],
-			},
-		},
-	},
-	plugins: [],
+    content: [
+        "./index.html",
+        "./src/**/*.{jsx, js}",
+        "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {
+            fontFamily: {
+                inter: ["inter", "serif"],
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
 };
