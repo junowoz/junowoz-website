4d2150d2a4db63f0463d56def968a5e5db8a229f
junowoz <junogouvea@gmail.com>
2023-05-09 12:40:51 -0400

    portfolio uodate 9 may

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -1,41 +1,49 @@
 export default [
-    {
-        year: "2023",
-        title: "Front-End Engineer",
-        company: "Re-Cycling",
-        duration: "Feb 2023 - Present",
-        details:
-            "• As a Front-End Developer at Re-Cycling, I use tools such as React, Nextjs, Zustand, and React-Bootstrap. I'm also responsible for developing and optimizing user-facing features, creating and maintaining reusable code, and ensuring the best performance, quality, and responsiveness of the online marketplace.",
-    },
-    {
-        year: "2022",
-        title: "Global Expansion Lead",
-        company: "WEB3DEV",
-        duration: "Oct 2022 - Present",
-        details:
-            "• Creation of a quarterly plan, and setting of community expansion goals for more than 5 Hispanic countries, recruiting and selection of potential collaborators, evaluation of team performance, providing constant feedback, and identification of which members are needed to achieve specific goals.",
-    },
-    {
-        year: "2022",
-        title: "Community Moderator",
-        company: "Modak",
-        duration: "Sep 2022 - Present",
-        details:
-            "• Review and moderate all user-generated content and user profiles, monitor conversations and work with contributors to manage, update, archive, or delete any inappropriate content.",
-    },
-    {
-        year: "2022",
-        title: "Software Development Intern",
-        company: "ECOA PUCRIO",
-        duration: "Mar 2022 - Mar 2023 · 1 yr",
-        details:
-            "• Front-end development with HTML, CSS, JavaScript, VueJS and ReactJS, research and problem-solving for the insurance industry, making use of emerging technologies, agile software development (Kanban & GTD), and multidisciplinary approach with Challenge-based learning (CBL) framework, App prototyping with Figma and design thinking methodologies.",
-    },
-    {
-        year: "2020",
-        title: "Computer Engineering, Economics",
-        duration: "5 years",
-        company: "Centro Universitário Fametro",
-        details: "Class Leader, Grade: 8.3",
-    },
+  {
+    year: "2023",
+    title: "Founder",
+    company: "EasySpot | LinkUp",
+    duration: "2023 - Present",
+    details:
+      "• I founded EasySpot and LinkUp as a result of my experience at Inovatec. EasySpot, utilizing IoR (Internet of Recognition) technology such as virtualized IoT sensors through machine vision, focuses on helping car users easily find parking spots in malls with a mobile app that indicates available parking spaces. LinkUp is an innovative tool for professionals who want to keep their resume, personal website, LinkedIn, and GitHub profiles up-to-date without manually updating each platform. With this app, you input your information once, and it will automatically update across all your relevant profiles.",
+  },
+  {
+    year: "2022",
+    title: "Global Expansion Lead",
+    company: "WEB3DEV",
+    duration: "Oct 2022 - Present",
+    details:
+      "• Creation of a quarterly plan, and setting of community expansion goals for more than 5 Hispanic countries, recruiting and selection of potential collaborators, evaluation of team performance, providing constant feedback, and identification of which members are needed to achieve specific goals.",
+  },
+  {
+    year: "2022",
+    title: "Community Moderator",
+    company: "Modak",
+    duration: "Sep 2022 - Present",
+    details:
+      "• Review and moderate all user-generated content and user profiles, monitor conversations and work with contributors to manage, update, archive, or delete any inappropriate content.",
+  },
+  {
+    year: "2023",
+    title: "Front-End Engineer",
+    company: "Re-Cycling",
+    duration: "Feb 2023 - Mar 2023 · 2 mos",
+    details:
+      "• As a Front-End Developer at Re-Cycling, I use tools such as React, Nextjs, Zustand, and React-Bootstrap. I'm also responsible for developing and optimizing user-facing features, creating and maintaining reusable code, and ensuring the best performance, quality, and responsiveness of the online marketplace.",
+  },
+  {
+    year: "2022",
+    title: "Software Development Intern",
+    company: "ECOA PUCRIO",
+    duration: "Mar 2022 - Mar 2023 · 1 yr",
+    details:
+      "• Front-end development with HTML, CSS, JavaScript, VueJS and ReactJS, research and problem-solving for the insurance industry, making use of emerging technologies, agile software development (Kanban & GTD), and multidisciplinary approach with Challenge-based learning (CBL) framework, App prototyping with Figma and design thinking methodologies.",
+  },
+  {
+    year: "2020",
+    title: "Computer Engineering, Economics",
+    duration: "5 years",
+    company: "Centro Universitário Fametro",
+    details: "Class Leader, Grade: 8.3",
+  },
 ];
