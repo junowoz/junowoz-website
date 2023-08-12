ab4b56920a8f13d17ba39425668cd92bb98fdf74
junowoz <junogouvea@gmail.com>
2023-08-12 18:45:34 -0400

    x

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBlogger, faSquareTwitter, faSquareGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faBlogger, faSquareXTwitter, faSquareGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Contact() {
   const [theme, setTheme] = React.useState("light");
@@ -49,7 +49,8 @@ function Contact() {
         rel="noopener noreferrer"
         className="p-1 inline-flex text-xl"
       >
-        <FontAwesomeIcon icon={faSquareTwitter} className="fa-2xl" />
+        
+        <FontAwesomeIcon icon={faSquareXTwitter} className="fa-2xl" />
       </a>
     </div>
   );
