e9aa42747585dc0614ca50ec36741a3c034ca220
ucloset <admin@ucloset.net>
2024-08-07 12:46:47 -0400

    add spotify iframe

---
+++ src/app/music/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Music() {
+  return (
+    <div id="music">
+      <iframe
+        src="https://open.spotify.com/embed/playlist/0GNSVsDb7h9a3UycirT6pk?utm_source=generator&theme=0"
+        width="100%"
+        height="512"
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </div>
+  );
+}
