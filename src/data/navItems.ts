21a262886ca6355b15490467bb1e82427104143e
ucloset <admin@ucloset.net>
2024-08-07 12:41:40 -0400

    add music section

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -4,5 +4,6 @@ export const navItems: NavItems[] = [
   { href: "/", label: "[INDEX]" },
   { href: "/books", label: "[BOOKS]" },
   { href: "/links", label: "[LINKS]" },
+  { href: "/music", label: "[MUSIC]" },
   { href: "https://blog.junowoz.com", label: "[BLOG]", external: true },
 ];
