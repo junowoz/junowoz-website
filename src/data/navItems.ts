04671ba90215e2531dfdc354fea04c2552a0c3e5
ucloset <admin@ucloset.net>
2024-08-08 02:34:59 -0400

    feat: Add blog page link to navigation

--- src/data/navItems.ts
+++ src/data/navItems.ts
@@ -2,8 +2,8 @@ import { NavItems } from "@/interfaces";
 
 export const navItems: NavItems[] = [
   { href: "/", label: "[INDEX]" },
+  { href: "/blog", label: "[BLOG]" },
   { href: "/books", label: "[BOOKS]" },
   { href: "/links", label: "[LINKS]" },
   { href: "/music", label: "[MUSIC]" },
-  { href: "https://blog.junowoz.com", label: "[BLOG]", external: true },
 ];
