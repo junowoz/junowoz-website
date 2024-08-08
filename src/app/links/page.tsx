b7dc2ff39dc3ef2b7c59875f26f13b27491b946d
ucloset <admin@ucloset.net>
2024-08-08 16:44:56 -0400

    feat: Update links page to use unordered list for better accessibility

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -10,14 +10,16 @@ export const metadata: Metadata = {
 export default function Links() {
   return (
     <div id="links">
-      {links.map((link, index) => (
-        <li key={index}>
-          <a href={link.url} className="text-link hover:underline">
-            {link.name}:{" "}
-          </a>
-          <span>{link.description}</span>
-        </li>
-      ))}
+      <ul className="list-disc list-inside">
+        {links.map((link, index) => (
+          <li key={index}>
+            <a href={link.url} className="text-link hover:underline">
+              {link.name}:{" "}
+            </a>
+            <span>{link.description}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
